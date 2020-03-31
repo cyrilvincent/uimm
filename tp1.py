364fd601071745be594bc2fb49ed1b1dc623ed10
cyrilvincent <contact@cyrilvincent.com>
2020-03-31 13:44:57 +0200

    UIMM 0.5/3

--- tp1.py
+++ tp1.py
@@ -4,3 +4,13 @@
 # Afficher votre age
 # Afficher votre age + 1
 # Afficher le reste de la division entière par 2 de votre age (22 % 2 => 0)
+
+annee = 2020
+naissance = input("Saisir votre année de naissance: ")
+naissance = int(naissance)
+print(naissance)
+age = annee - naissance
+print(f"Vous avez {age} ans")
+age2 = age + 1
+print(f"L'année prochaine vous aurez {age2} ans")
+print(age % 2)

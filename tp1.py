2f2a29e54a6b44053679df5c4696f476d19e042f
cyrilvincent <contact@cyrilvincent.com>
2020-03-31 12:03:27 +0200

    UIMM 0.5/3

---
+++ tp1.py
@@ -0,0 +1,6 @@
+# Créer tp1
+# Créer une variable annee = 2020
+# Saisir votre année de naissance
+# Afficher votre age
+# Afficher votre age + 1
+# Afficher le reste de la division entière par 2 de votre age (22 % 2 => 0)

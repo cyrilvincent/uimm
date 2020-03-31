9e0f78e5a4a5e08454bd00a59148a387e037366a
cyrilvincent <contact@cyrilvincent.com>
2020-03-31 11:57:06 +0200

    UIMM 0.5/3

---
+++ hello.py
@@ -0,0 +1,22 @@
+print("Hello World!")
+x = 1
+#print(x)
+#gjghrgjkh
+x = 11//4
+print(x)
+print(11%4)
+print(1115457%2)
+z = "toto"
+print(z)
+z = "toto" + "titi"
+print(z)
+z = 13+12
+print(z)
+z = "13"+"12"
+print(z)
+z = "Hello Python"
+print(z[0])
+print(len(z))
+x = input("Veuillez saisir un entier: ")
+x = int(x)
+print(x + 12)

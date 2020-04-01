ebe1315d6f4de83a4c565e03cb814ca995bb5c61
cyrilvincent <contact@cyrilvincent.com>
2020-04-01 15:34:46 +0200

    UIMM 2/3

---
+++ premier.py
@@ -0,0 +1,16 @@
+# Créer la fonction estPremier(n) qui renvoie 1 si n est premier 0 sinon
+# estPremier(7) => 1
+# estPremier(8) => 0
+# %
+# Un nombre premier est un nombre divisible uniquement par 1 et par lui même
+# 0 et 1 ne sont pas premier
+# 2,3,5,7,11,13,17,...
+# Un nombre n n'est pas premier s'il possède un diviseur entre 2 et n-1
+
+# Créer la fonction afficherPremiers(n) et qui affiche tous les nombres premiers entre 2 et n
+# afficherPremiers(10) => 2,3,5,7
+
+# Faire la fonction cryptage qui crypte un nombre par sa clé, vérifier que la clé est un nombre premier
+# cryptage(100,7) => 700
+# cryptage(100,8) => -1
+# decryptage(700, 7) => 100

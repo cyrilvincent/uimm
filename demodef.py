1eb0116c117d80bba268df347b39678ff3b20574
cyrilvincent <contact@cyrilvincent.com>
2020-04-01 10:41:33 +0200

    UIMM 1.25/3

---
+++ demodef.py
@@ -0,0 +1,35 @@
+# def incremente(x):
+#     return x + 1
+#
+# resultat = incremente(1)
+# print(resultat)
+# resultat = incremente(55)
+# print(resultat)
+# print(incremente(88))
+#
+# def toto():
+#     print("toto")
+#
+# def echo(mot):
+#     print(mot)
+#
+# def add(x,y):
+#     return x + y
+
+def max(x,y):
+    if x > y:
+        return x
+    else:
+        return y
+
+def printn(n, msg):
+    for i in range(n):
+        print(msg)
+
+# toto()
+# echo("coucou")
+# print(add(2,3))
+# print(max(2,3))
+# res = max(2,3)
+# printn(10, "coucou")
+
---
+++ tp5.py
@@ -0,0 +1,4 @@
+# Faire la fonction fact(n)
+# Faire la fonction sumarithmetique(n) sumarithmetique(5) = 5 + 4 + 3 + 2 +1 = 15
+# Facultatif Faire la fonction calculImpot(salaire)
+# Mettre toutes les fonctions dans un fichier nommé librairie.py et tester ces fonctions depuis tp5

7f7a458068aa541191b16e4ec46df88e39a49d51
cyrilvincent <contact@cyrilvincent.com>
2020-04-03 13:51:18 +0200

    UIMM 3/3

---
+++ UIMMWindows/UIMMWindows/Form2.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UIMMWindows
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s = textBox1.Text;
+            int n = Convert.ToInt32(s);
+            bool b = isPrime(n);
+            if(b == true)
+            {
+                textBox2.Text = s + " est premier";
+            }
+            else
+            {
+                textBox2.Text = s + " n'est pas premier";
+            }
+        }
+
+        private bool isPrime(int n)
+        {
+            if(n<2)
+            {
+                return false;
+            }
+            else
+            {
+                for(int i=2;i<n;i++)
+                {
+                    if(n % i == 0) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+    }
+}

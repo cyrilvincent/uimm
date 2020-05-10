c8cd6a9571d1db942111c5968410e55540be0605
cyrilvincent <contact@cyrilvincent.com>
2020-05-10 17:55:33 +0200

    AFPI 2

--- tp5.py
+++
@@ -1,9 +0,0 @@
-# Faire la fonction fact(n)
-# Faire la fonction sumarithmetique(n) sumarithmetique(5) = 5 + 4 + 3 + 2 +1 = 15
-# Facultatif Faire la fonction calculImpot(salaire)
-# Mettre toutes les fonctions dans un fichier nommé librairie.py et tester ces fonctions depuis tp5
-
-import librairie
-print(librairie.fact(5))
-print(librairie.sumarithmetique(5))
-print(f"Vous allez payer {librairie.impots(30000)}")

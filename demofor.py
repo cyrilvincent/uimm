ab606fb5fa94ef46319761c9388a2c8b44b6f0d5
cyrilvincent <contact@cyrilvincent.com>
2020-05-11 14:12:01 +0200

    UIMM 0.75/3

---
+++ demofor.py
@@ -0,0 +1,8 @@
+# for i in range(10): # 0 à 9
+#     print(i)
+
+# for i in range(10,20): # 10 à 20 non inclus
+#     print(i)
+
+for i in range(20,10,-2):
+    print(i)
--- tp2.py
+++ tp2.py
@@ -1,5 +1,30 @@
 # While
 # Faire une boucle de 0 à 1000 avec un pas de 3
 # Afficher les puissances de 2 entre 0 et 16
-# Afficher un compte à rebourd de 10 à 0 inclus
+# Afficher un compte à rebours de 10 à 0 inclus
 # Afficher la factorielle de n : n! = n * (n - 1)! : 5! = 5 * 4 * 3 * 2 * 1 = 120, 4! = 4 * 3 * 2 * 1 = 24
+
+i = 0
+while i < 1000:
+    print(i)
+    i += 3
+
+i = 0
+while i <= 16:
+    print(2 ** i)
+    i += 1
+
+i = 10
+while i >= 0:
+    print(i)
+    i -= 1
+
+n = int(input("N: ")) # 5! = 5 * 4 * 3 * 2 * 1
+i = n
+res = 1
+while i >= 1 :
+    res = res * i
+    i = i - 1
+print(res)
+
+# Refaire le tp2 avec un for

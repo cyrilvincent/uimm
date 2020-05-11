16b410396983a9f191d35055546d3269782b05c0
cyrilvincent <contact@cyrilvincent.com>
2020-05-11 15:20:13 +0200

    UIMM 1/3

--- tp3.py
+++ tp3.py
@@ -4,3 +4,32 @@
 # Faire la fonction isEven(i) qui retourne True ou False si i est pair ou impair
 # Faire la fonction isBissextile(i) qui retourne True si i est bissextile
 # Faire la fonction fact(n) qui retourne n!
+
+def add(i,j):
+    return i + j
+
+def max(i,j):
+    if i > j:
+        return i
+    else:
+        return j
+
+def isEven(i):
+    if i % 2 == 0:
+        return True
+    else:
+        return False
+
+def isBissextile(i):
+    if (i % 4 == 0 and not i % 100 == 0) or i % 400 == 0:
+        return True
+    else:
+        return False
+
+def fact(n):
+    res = 1
+    for i in range(n, 0, -1):
+        res = res * i
+    return res
+
+print(fact(5))
---
+++ tp4.py
@@ -0,0 +1,22 @@
+# Une nombre premier est un nombre divisible uniquement par 1 et lui-même
+# Le premier nombre premier est 2
+# 2,3,5,7,11,13,17,19
+# La fonction isPrime(x) qui retourne True si x est premier
+# Afficher les 1000 premiers nombres premiers
+
+# fibonacci(n) :
+# fibonacci(0) = 0
+# fibonacci(1) = 1
+# fibonacci(n) = fibonacci(n - 1) + fibonacci(n - 2)
+# fibonacci(2) = f(1) + f(0) = 1 + 0 = 1
+
+# Impots
+# 14 % pour la fraction supérieure à 10 064 € et inférieure ou égale à 27 794 €
+# 30 % pour la fraction supérieure à 27 794 € et inférieure ou égale à 74 517 €
+# 41 % pour la fraction supérieure à 74 517 € et inférieure ou égale à 157 806 €
+# 45 % pour la fraction supérieure à 157 806 €
+# 15064€ sur vos 10064€ vous payez 0% d'impots, vous payez 14% de 15064-10064=5000 soit 700€ soit 700/15064 = 4.6%
+# 160000€ sur 10064=>0% sur 27794-10064=>14% sur 74517-27794=>30% sur 157806-74517=>41% sur 160000-157806=>45%
+# Faire la fonction calcImpots(revenuNet) => montant à payer
+# Puis calculer le % réel d'impôts
+

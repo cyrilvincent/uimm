0176104ef9d543a4e8b5bdcd1ba691fb7325d006
cyrilvincent <contact@cyrilvincent.com>
2020-05-11 13:21:03 +0200

    UIMM 0.5/3

--- tp1.py
+++ tp1.py
@@ -14,3 +14,24 @@ print(age % 2)
 # Afficher si votre age est pair ou impair
 # Afficher votre catégorie d'age (<2 BB, <12 Enfant, < 18 Ado, <62 Adulte, Retraité)
 # Affichée si votre année de naissance est bissextile d'après wikipedia
+
+if age % 2 == 0:
+    print("Votre age est pair")
+else:
+    print("Votre age est impair")
+
+if age < 2:
+    print("Vous êtes un BB")
+elif age < 12:
+    print("Vous êtes un enfant")
+elif age < 18:
+    print("Vous êtes un ado")
+elif age < 62:
+    print("Vous êtes un adulte")
+else:
+    print("Vous êtes un retraité")
+
+if (birth % 4 == 0 and not birth % 100 == 0) or (birth % 400 == 0):
+    print("Vous êtes né une année bissextile")
+else:
+    print("Vous êtes né une année non bissextile")

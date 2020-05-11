b79bf6d5efcbe5bdaa91674602d13e471ae5dfcc
cyrilvincent <contact@cyrilvincent.com>
2020-05-11 11:16:38 +0200

    UIMM 0.25/3

--- hello.py
+++ hello.py
@@ -1,5 +1,30 @@
-print("Hello world!")
+print("Hello world!") # string
 # File > New > Python File
 # Click droit sur hello.py > run
 # Cliquer sur la fleche verte en haut à droite
 # Dans l'invité de commande taper python hello.py
+print((3+2)*4**10)
+print(3/2)
+print(3//2)
+print(3%2) #Modulo Reste de la division euclidienne
+print(5//2)
+print(5%2)
+x = 4
+print(x%2)
+toto = 3.14
+TOTO = 4
+x = input("Veuillez saisir un chaine de caractère: ")
+#print(len(x))
+# <=>
+y=len(x)
+print(y)
+print(y % 2)
+a = int(input("Veuillez saisir un chiffre: "))
+print(a)
+print(a ** 2)
+s = "toTO "
+print(s)
+print(s.lower())
+print(s.upper())
+print(s.capitalize())
+print(s.strip())
---
+++ tp1.py
@@ -0,0 +1,8 @@
+# Créer la variable year = 2020
+# Saisir votre année de naissance dans une variable entière
+# Afficher votre année de naissance
+# Afficher votre age de l'année en cours (year)
+# Afficher votre age de l'année prochaine
+# Afficher le modulo 2 de votre age
+x = 18
+print(f"Vous avez {x} ans")

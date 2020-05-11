fb960978a47d5d9f96b85f91147465adbe95f135
cyrilvincent <contact@cyrilvincent.com>
2020-05-11 10:17:35 +0200

    UIMM 0.25/3

---
+++ hello.py
@@ -0,0 +1,5 @@
+print("Hello world!")
+# File > New > Python File
+# Click droit sur hello.py > run
+# Cliquer sur la fleche verte en haut à droite
+# Dans l'invité de commande taper python hello.py

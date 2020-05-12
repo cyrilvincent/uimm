612cd75f0ba2a6f799ed9e9d8fdb87ac67d911aa
cyrilvincent <contact@cyrilvincent.com>
2020-05-12 14:37:52 +0200

    UIMM 1.75/3

---
+++ demomatplotlib.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+import csv
+
+# File > Settings > Project > Project Interpreter
+# Cliquer sur l'icone +, Saisir Matplotlib, sélectionner et installer
+
+#plt.plot(range(100), range(100))
+#plt.scatter(range(100), range(100))
+#plt.show()
+
+with open("house.csv") as f:
+    reader = csv.DictReader(f)
+    surfaces = []
+    loyers = []
+    for row in reader:
+        s = int(row["surface"])
+        l = int(row["loyer"])
+        surfaces.append(s)
+        loyers.append(l)
+print(surfaces)
+print(loyers)
+
+plt.scatter(surfaces, loyers)
+#plt.bar(surfaces, loyers)
+plt.show()
--- tp7.py
+++ tp7.py
@@ -1,13 +1,66 @@
 import csv
-with open("house.csv") as f:
-    reader = csv.DictReader(f)
-    for row in reader:
-        s = int(row["surface"])
-        l = int(row["loyer"])
-        print(l / s)
+
 
 # Faire une fonction displayLoyerPerM2(file) => afficher les loyers par m2
 # Faire une fonction calcSurfaceMoyenne(file) => return surface moyenne 10,20,30 => 20
 # Faire une fonction calcLoyerMoyen(file) => return loyer moyen 10,20,30 => 20
 # Faire une fonction getLoyerPerM2(file) => 10,100 20,350 30,300 => [10, 17.5, 10]
 # Calculer le loyer moyen par m2, le min , max
+
+def displayLoyerPerM2(file):
+    with open(file) as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            s = int(row["surface"])
+            l = int(row["loyer"])
+            print(l / s)
+
+#displayLoyerPerM2("house.csv")
+
+def calcSurfaceMoyenne(file):
+    with open(file) as f:
+        reader = csv.DictReader(f)
+        res = 0
+        nb = 0
+        for row in reader:
+            s = int(row["surface"])
+            res += s
+            nb += 1
+        return res / nb
+
+print(calcSurfaceMoyenne("house.csv"))
+
+def calcLoyerMoyen(file):
+    with open(file) as f:
+        reader = csv.DictReader(f)
+        res = 0
+        nb = 0
+        for row in reader:
+            s = int(row["loyer"])
+            res += s
+            nb += 1
+        return res / nb
+
+print(calcLoyerMoyen("house.csv"))
+
+def getLoyerPerM2(file):
+    res = []
+    with open(file) as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            s = int(row["surface"])
+            l = int(row["loyer"])
+            res.append(l / s)
+    return res
+
+def sum(l):
+    res = 0
+    for val in l:
+        res += val
+    return res
+
+def average(l):
+    return sum(l)/len(l)
+
+l = getLoyerPerM2("house.csv")
+print(average(l))

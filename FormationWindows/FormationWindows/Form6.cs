8a5c5fdc799e5a0ee7a366ffc86d96ad136b69bf
cyrilvincent <contact@cyrilvincent.com>
2020-05-13 13:44:26 +0200

    UIMM 2.75/3

---
+++ FormationWindows/FormationWindows/Form6.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormationWindows
+{
+    public partial class Form6 : Form
+    {
+        public Form6()
+        {
+            InitializeComponent();
+        }
+
+        private void Form6_Load(object sender, EventArgs e)
+        {
+            // Vous allez générer aléatoirement un nombre entier entre 1 et 1000
+            // Vous allez essayer de le deviner
+            // Saisir un nombre
+            // Cliquer sur un bouton
+            // Le programme trop haut ou trop bas ou vous avez gagné
+            // Afficher le nombre de coup total
+            // Limiter le nombre de coup total, par exemple 10 coups
+            // Facultatif : Afficher une picturebox quand vous gagnez et une autre quand vous perdez, .visible=true false
+        }
+    }
+}

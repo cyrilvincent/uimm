11f62d15023fcc05222d48bacb44956473fecdfd
cyrilvincent <contact@cyrilvincent.com>
2020-05-13 09:26:24 +0200

    UIMM 2.25/3

--- tp8.py
+++ tp8.py
@@ -3,3 +3,31 @@
 # Pour AM et VM calculer la moyenne (100,0), le min (1,-240), le max (200,+240)
 # Afficher AM et VM
 # Detecter les erreurs abs(VM - VT) > 1 : afficher ces erreurs
+import csv
+
+with open("mesures.csv") as f:
+    reader = csv.DictReader(f)
+    AT = []
+    AM = []
+    VT = []
+    VM = []
+    for row in reader:
+        AT.append(float(row["AT"]))
+        AM.append(float(row["AM"]))
+        VT.append(float(row["VT"]))
+        VM.append(float(row["VM"]))
+
+print(AT)
+print(AM)
+print(VT)
+print(VM)
+
+print(min(AM), max(AM), sum(AM)/len(AM))
+print(min(VM), max(VM), sum(VM)/len(VM))
+
+for i in range(len(VM)):
+    error = abs(VM[i] - VT[i])
+    if error > 1:
+        print(i, VM[i], VT[i], error)
+
+
